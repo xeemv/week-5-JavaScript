f3edf5c7ef02ab7f07998e948a02b816c2592348
xeemv <xeeyang08@gmail.com>
2022-01-10 13:03:11 -0600

    finished video

--- m.js
+++ m.js
@@ -111,7 +111,7 @@ class Menu {
             - we'll want to place this line code "selection = this.showMainMenuOptions();" here again while the user is still inside of the loop.
             - keep looping as long as the user does not select zero or something other than 1-4.
             */
-       }
+       } // stopped at the 12.42 minute mark in the video
        alert('Goodbye!'); 
         /*
         - this will alert the user if they did select zero
@@ -126,13 +126,13 @@ class Menu {
 
     // will implement our methods that we were going to have/show the user.
    showMainMenuOptions(){ 
-       return prompt(`
-       0) exist
-       1) create new team
-       2) view team
-       3) delete team
-       4) display all teams
-       `);
+        return prompt(`
+            0) exist
+            1) create new team
+            2) view team
+            3) delete team
+            4) display all teams
+        `);
        /* 
        - method to show prompt = a pop-up box of what the menu options (switches section) are that will ask the user for input.
        - return is the input that comes back from that prompt
@@ -142,7 +142,18 @@ class Menu {
        */
    }
 
-   displayTeams(){
+   // view a specific team
+   showTeamMenuOptions(teamInfo){
+       return prompt(`
+            0) back
+            1) create player
+            2) delete player
+            -------------------
+            ${teamInfo}
+        `);
+   }
+
+   displayTeams(){  
        let teamString = '';
        for (let i = 0; i < this.teams.length; i++) { 
         teamString += i + ') ' + this.teams[i].name + '\n';
@@ -152,7 +163,87 @@ class Menu {
             - grab each team
             - get the name for that specific team
             - add a new line so that all the team names will show up w/ an index numbering them w/ their name on a different line
-        */
+            */
+        alert(teamString); // to display all the teams
    }
+   createTeam() {
+       let name = prompt('Enter name for new team:'); // prompt user to enter the name they want to use for the team
+       this.teams.push(new Team(name)); // creating a new instance of an object of a class and create a new array
+   }
+
+   viewTeam(){
+       // to view the details of a specific team
+       let index = prompt('Enter the index of the team you wish to view:');
+       if (index > -1 && index < this.teams.length){
+           /*
+           - this if statement is going to do some validation here
+           - if the team was less than zero or greater than the length of our teams array = error
+                - we dont want to validate the user input
+                - because the users are unpredictable and may input anything in
+                - this will avoid crashing and errors
+           */
+          this.selectedTeam = this.teams[index];
+          let description = 'Team Name: ' + this.selectedTeam.name + '\n';
+
+          for (let i = 0; i < this.selectedTeam.players.length; i++) {
+              description += i + ') ' + this.selectedTeam.players[i].name + ' - ' + this.selectedTeam.players[i].position + '\n';
+              // this will build up the list of all the team players and their position
+          }
+
+
+
+          /*  to show the menu options: see line 190
+        - "top-down development approach" means:
+            - we'll start using methods that don't exist yet to build out our menu by:
+                - what we think it is going to look like
+                - then implement those methods
+        */
+          let selection = this.showTeamMenuOptions(description);
+          switch (selection){
+              // this is a sub menu of the full menu **they are not the same**
+            case '1':
+                this.createPlayer();
+                break;
+            case '2':
+                this.deletePlayer();
+                // no need to add a break here because nothing will come after this      
+          }
+       }
+   }
+
+   // delete team
+   deleteTeam(){
+        let index = prompt('Enter the index of the team you wish to delete:');
+        if (index > -1 && index < this.teams.length) {
+        this.teams.splice(index, 1);
+        }
+   }
+
+   createPlayer(){
+       let name = prompt('Enter name for new player:');
+       let position = prompt('Enter position for new player:');
+       this.selectedTeam.players.push(new Player(name,position));
+   }
+
+   deletePlayer(){
+       let index = prompt('Enter the index of the player you wish to delete:');
+       if (index > -1 && index < this.selectedTeam.players.length){
+           this.selectedTeam.players.splice(index, 1);
+       }
+   }
+
 }
- // stopped at 12.42 minute mark on the video
+ 
+/*
+- we need to test this by creating an instance of our menu
+*/
+
+let menu = new Menu();
+menu.start();
+/*
+- menu.start(); ---->
+    - the method that shows everything
+
+- make sure that if you are adding a new method
+    - you are currently outside of any other method by checking the curly brackets and it is not w/in another method
+*/
